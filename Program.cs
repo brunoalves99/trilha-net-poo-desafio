fd7209ed88dc0a07cd4de4adfce8bf14dec71402
Bruno Saturnino Alves <brunosaturnino55@gmail.com>
2023-01-10 19:25:02 -0300

    Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("97653322", "Modelo 1", "223", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("88223344", "Modelo 2", "332", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
